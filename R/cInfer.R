062ecac9d38e3695ea9a991ddf5d419cde3a5847
Muxuan Liang <10004752+muxuanliang@users.noreply.github.com>
2020-05-28 22:17:56 -0700

    hypothesis testing and interval estimation done

---
+++ R/cInfer.R
@@ -0,0 +1,102 @@
+#cInfer implement the decorrelated score for testing and interval estimation
+cInfer <- function(x, y, coef = NULL, lossType='logistic', parallel = TRUE, indexToTest = NULL, intercept=FALSE, ...){
+  #set sample size
+  n <- length(y)
+  # if coef=NULL
+  if (is.null(coef)){
+    fit <- cv.cLearn(x=x, y=y, lambdaSeq = NULL, lossType = lossType, ...)
+    coef <- fit$fit$coef[,fit$lambda.seq==fit$lambda.opt]
+  }
+
+  # if indextoTest is null
+  if (is.null(indexToTest)){
+    indexToTest <- c(1:8)
+  }
+
+  # set y if y is not 1 or -1
+  if (as.numeric(levels(factor(y))[1]) != -1){
+    y[factor(y)==levels(factor(y))[1]] <- -1
+  }
+  if (as.numeric(levels(factor(y))[2]) != 1){
+    y[factor(y)==levels(factor(y))[2]] <- 1
+  }
+  # fit w
+  fit_w <- NULL
+  score <- rep(NA, times=length(indexToTest))
+  sigma <- rep(NA, times=length(indexToTest))
+  coefAN <- rep(NA, times=length(indexToTest))
+  I <- rep(NA, times=length(indexToTest))
+  if (!parallel){
+    for (index in 1:length(indexToTest)){
+      # set pseudo data
+      pseudo.x <- x[, -indexToTest[index]]
+      pseudo.y <- x[,indexToTest[index]]
+      # set weight
+      weights <- loss(y * x %*% coef, lossType=lossType, order=2, ...)
+      #fit w
+      fit_w[[index]] <- glmnet::cv.glmnet(x=pseudo.x, y=pseudo.y, weights = weights, intercept = intercept, standardize = FALSE)
+      link_w <- predict(fit_w[[index]], newx = pseudo.x, s=fit_w[[index]]$lambda.min)
+      # set beta null
+      coefNULL <- coef
+      coefNULL[indexToTest[index]] <- 0
+      # get score under null
+      linkNULL <- x %*% coefNULL
+      scoreWeightNULL <- y * loss(y * linkNULL, lossType=lossType, order=1, ...)
+      tmpNULL <- scoreWeightNULL * (pseudo.y-link_w)
+      score[index] <- mean(tmpNULL)
+      # set betaAN
+      link <- x %*% coef
+      scoreWeight <- y * loss(y * link, lossType=lossType, order=1, ...)
+      tmp <- scoreWeight * (pseudo.y-link_w)
+      Itmp <- loss(y * link, lossType=lossType, order=2, ...) * pseudo.y * (pseudo.y - link_w)
+      coefAN[index] <- coef[indexToTest[index]]-mean(tmp) /(mean(Itmp))
+      sigma[index] <- sqrt(mean((tmp)^2))
+      I[index] <- (mean(Itmp)*2)
+
+      sigma[index] <- sqrt(mean((tmpNULL)^2))
+    }
+  } else {
+    # set multi-thread
+    library(doParallel)
+    n_cores <- detectCores(all.tests = FALSE, logical = TRUE)
+    cl <- makeCluster(min(length(indexToTest), n_cores))
+    registerDoParallel(cl)
+
+    #calculate
+    res <- foreach(index=indexToTest,.packages = 'glmnet') %dopar%{
+      pseudo.x <- x[, -indexToTest[index]]
+      pseudo.y <- x[, indexToTest[index]]
+      # set weight
+      weights <- loss(y * x %*% coef, lossType=lossType, order=2, ...)
+      #fit w
+      fit_w <- glmnet::cv.glmnet(x=pseudo.x, y=pseudo.y, weights = weights, intercept = intercept, standardize = FALSE)
+      link_w <- predict(fit_w, newx = pseudo.x, s=fit_w$lambda.min)
+      # set beta null
+      coefNULL <- coef
+      coefNULL[indexToTest[index]] <- 0
+      # get score under null
+      linkNULL <- x %*% coefNULL
+      scoreWeightNULL <- y * loss(y * linkNULL, lossType=lossType, order=1, ...)
+      tmpNULL <- scoreWeightNULL * (pseudo.y-link_w)
+      score <- mean(tmpNULL)
+      # set betaAN
+      link <- x %*% coef
+      scoreWeight <- y * loss(y * link, lossType=lossType, order=1, ...)
+      tmp <- scoreWeight * (pseudo.y-link_w)
+      Itmp <- loss(y * link, lossType=lossType, order=2, ...) * pseudo.y * (pseudo.y - link_w)
+      coefAN <- coef[indexToTest[index]]-mean(tmp) /(mean(Itmp))
+      sigma <- sqrt(mean((tmp)^2))
+      I <- (mean(Itmp)*2)
+      sigma <- sqrt(mean((tmpNULL)^2))
+      list(fit_w = fit_w, score=score, sigma=sigma, coefAN=coefAN, I=I)
+    }
+    stopCluster(cl)
+    for (index in indexToTest){
+      score[index] <- res[[index]]$score
+      sigma[index] <- res[[index]]$sigma
+      coefAN[index] <- res[[index]]$coefAN
+      I[index] <- res[[index]]$I
+    }
+  }
+  list(fit=fit, coef=coef, indexToTest=indexToTest, pvalue=pnorm(-abs(sqrt(n)*score/sigma))*2, coefAN=coefAN, sigmaAN=sigma/I)
+}
--- test.R
+++ test.R
@@ -4,5 +4,9 @@ p <- 500
 x <- array(rnorm(nobs*p), c(nobs, p))
 beta_true <- c(1,-1,0.5,-0.5, rep(0, times=p-4))
 y <- apply(x, 1, function(t){rbinom(1,1, prob = exp(t%*%beta_true)/ (1+exp(t%*%beta_true)))})
-fit <- cv.cLearn(x, y, weight = c(0, 0, 0, 0, rep(1, times= p-4)), lossType = 'logistic', tol = 1e-3)
-fit$fit$coef[,fit$lambda.seq==fit$lambda.opt]
+fit <- cInfer(x, y, weight = c(0, 0, 0, 0, rep(1, times= p-4)), lossType = 'smoothed_hinge', tol = 1e-3)
+fit$coef
+fit$coef[fit$indexToTest]
+fit$pvalue
+fit$coefAN
+1.96*fit$sigmaAN/sqrt(nobs)
