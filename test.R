dabc03824202edd9f8d52ae66d500f024dbadee1
Muxuan Liang <10004752+muxuanliang@users.noreply.github.com>
2020-06-27 14:29:03 -0700

    update the test example

--- test.R
+++ test.R
@@ -3,11 +3,13 @@ nobs <- 500
 p <- 500
 x <- array(rnorm(nobs*p), c(nobs, p))
 beta_true <- c(1,-1,0.5,-0.5, rep(0, times=p-4))
-y <- apply(x, 1, function(t){rbinom(1,1, prob = exp(t%*%beta_true)/ (1+exp(t%*%beta_true)))})
+y <- apply(x, 1, function(t){rpois(1,lambda = exp(t%*%beta_true))}) + rnorm(nobs)
+cutoff <- quantile(y)[2:4]
+y.cutoff <- list(y>cutoff[1], y>cutoff[2], y>cutoff[3])
 
 ## fit using smoothed_hinge
-system.time(fit <- cInfer(x, y, weight = c(0, 0, 1, 1, rep(1, times= p-4)), lossType = 'smoothed_hinge', tol = 1e-3, parallel = FALSE))
-system.time(fit <- cInfer(x, y, weight = c(0, 0, 1, 1, rep(1, times= p-4)), lossType = 'smoothed_hinge', tol = 1e-3))
+system.time(fit <- cInfer(x, y=y.cutoff, weight = c(1, 1, 1, 1, rep(1, times= p-4)), lossType = 'smoothed_hinge', tol = 1e-3, parallel = FALSE))
+system.time(fit <- cInfer(x, y=y.cutoff, weight = c(1, 1, 1, 1, rep(1, times= p-4)), lossType = 'smoothed_hinge', tol = 1e-3))
 ## estimated coef
 fit$coef
 
@@ -24,8 +26,8 @@ fit$coefAN
 #2* 1.96*fit$sigmaAN/sqrt(nobs) / abs(fit$coefAN)
 
 ## fit using logistic
-system.time(fit <- cInfer(x, y, weight = c(0, 0, 1, 1, rep(1, times= p-4)), lossType = 'logistic', tol = 1e-3, parallel = FALSE))
-system.time(fit <- cInfer(x, y, weight = c(0, 0, 1, 1, rep(1, times= p-4)), lossType = 'logistic', tol = 1e-3))
+system.time(fit <- cInfer(x, y=y.cutoff, weight = c(1, 1, 1, 1, rep(1, times= p-4)), lossType = 'logistic', tol = 1e-3, parallel = FALSE))
+system.time(fit <- cInfer(x, y=y.cutoff, weight = c(1, 1, 1, 1, rep(1, times= p-4)), lossType = 'logistic', tol = 1e-3))
 ## estimated coef
 fit$coef
 
