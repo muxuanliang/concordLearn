90a8e499ddffcb1b0d4349fddb0dab6b2cac4887
Muxuan Liang <10004752+muxuanliang@users.noreply.github.com>
2020-07-16 11:28:11 -0700

    update test.R

--- test.R
+++ test.R
@@ -1,4 +1,4 @@
-simulation <- function(sample.size = 500, p = 500){
+simulation <- function(sample.size = 500, p = 500, alpha=0){
   print(system.time(res <- foreach(index = 1:500,.packages = c('concordLearn'),.combine = rbind,.errorhandling='remove')%dopar%{
   ## test
   set.seed(index)
@@ -6,41 +6,48 @@ simulation <- function(sample.size = 500, p = 500){
   p <- p
   x <- array(rnorm(nobs*p), c(nobs, p))
   beta_true <- c(1,-1,1,-1, rep(0, times=p-4))
-  y <- 5*pnorm(x%*%beta_true) + rnorm(nobs)
-  cutoff <- quantile(y)[c(2,4)]
-  y.cutoff <- list(y>cutoff[1], y>cutoff[2])
+  beta_modify <- c(1,1,1,1, rep(0, times=p-4))
+  U <- 5*pnorm(x%*%beta_true) + rnorm(nobs)
+  y1 <- U
+  y2 <- (1-alpha) * U+alpha * (5*pnorm(-x%*%beta_modify) + rnorm(nobs))
+  cutoff <- quantile(U)[c(2,4)]
+  y.cutoff <- list(y1>cutoff[1], y2>cutoff[2])
+
+  # concord score
+  #(mean(y.cutoff[[1]]*y.cutoff[[2]])-mean(y.cutoff[[1]])*mean(y.cutoff[[2]]))/var(y.cutoff[[2]])
 
   ## fit using smoothed_hinge
-  system.time(fit_hinge_trans <- cInfer(x, y=y.cutoff, y_refit = list(y>cutoff[1]), weight = c(1, 1, 1, 1, rep(1, times= p-4)), lossType = 'smoothed_hinge', tol = 1e-3, parallel = FALSE))
-  system.time(fit_hinge <- cInfer(x, y=list(y>cutoff[1]), weight = c(1, 1, 1, 1, rep(1, times= p-4)), lossType = 'smoothed_hinge', tol = 1e-3, parallel = FALSE))
+  #system.time(fit_hinge_trans <- cInfer(x, y=y.cutoff, y_refit = list(y>cutoff[1]), weight = c(1, 1, 1, 1, rep(1, times= p-4)), lossType = 'smoothed_hinge', tol = 1e-3, parallel = FALSE))
+  #system.time(fit_hinge <- cInfer(x, y=list(y>cutoff[1]), weight = c(1, 1, 1, 1, rep(1, times= p-4)), lossType = 'smoothed_hinge', tol = 1e-3, parallel = FALSE))
 
   ## fit using logistic
-  system.time(fit_logistic_trans <- cInfer(x, y=y.cutoff, y_refit = list(y>cutoff[1]), weight = c(1, 1, 1, 1, rep(1, times= p-4)), lossType = 'logistic', tol = 1e-3, parallel = FALSE))
-  system.time(fit_logistic <- cInfer(x, y=list(y>cutoff[1]), weight = c(1, 1, 1, 1, rep(1, times= p-4)), lossType = 'logistic', tol = 1e-3, parallel = FALSE))
+  system.time(fit_logistic_trans <- cInfer(x, y=y.cutoff, y_refit = list(y.cutoff[[1]]), weight = c(1, 1, 1, 1, rep(1, times= p-4)), lossType = 'logistic', tol = 1e-3, parallel = FALSE))
+  system.time(fit_logistic_comb <- cInfer(x, y=y.cutoff, weight = c(1, 1, 1, 1, rep(1, times= p-4)), lossType = 'logistic', tol = 1e-3, parallel = FALSE))
+  system.time(fit_logistic <- cInfer(x, y=list(y.cutoff[[1]]), weight = c(1, 1, 1, 1, rep(1, times= p-4)), lossType = 'logistic', tol = 1e-3, parallel = FALSE))
 
   # testing data
   x.test <- array(rnorm(10^4*p), c(10^4, p))
   y.test.nonoise <- 5*pnorm(x.test%*%beta_true)
   y.test <- y.test.nonoise + rnorm(10^4)
   y.test.label <- (y.test>cutoff[1])
-  score.class.hinge.trans <- mean((x.test %*% fit_hinge_trans$coef > fit_hinge_trans$off.set) == y.test.label)
-  score.class.hinge <- mean((x.test %*% fit_hinge$coef > fit_hinge$off.set) == y.test.label)
+  #score.class.hinge.trans <- mean((x.test %*% fit_hinge_trans$coef > fit_hinge_trans$off.set) == y.test.label)
+  #score.class.hinge <- mean((x.test %*% fit_hinge$coef > fit_hinge$off.set) == y.test.label)
   score.class.logistic.trans <- mean((x.test %*% fit_logistic_trans$coef > fit_logistic_trans$off.set) == y.test.label)
+  score.class.logistic.comb <- mean((x.test %*% fit_logistic_comb$coef > fit_logistic_comb$off.set[1]) == y.test.label)
   score.class.logistic <- mean((x.test %*% fit_logistic$coef > fit_logistic$off.set) == y.test.label)
-  score.rank.hinge.trans <- cor(x.test %*% fit_hinge_trans$coef,y=y.test.nonoise, method='kendall')
-  score.rank.hinge <- cor(x.test %*% fit_hinge$coef,y=y.test.nonoise, method='kendall')
+  #score.rank.hinge.trans <- cor(x.test %*% fit_hinge_trans$coef,y=y.test.nonoise, method='kendall')
+  #score.rank.hinge <- cor(x.test %*% fit_hinge$coef,y=y.test.nonoise, method='kendall')
   score.rank.logistic.trans <- cor(x.test %*% fit_logistic_trans$coef,y=y.test.nonoise, method='kendall')
+  score.rank.logistic.comb <- cor(x.test %*% fit_logistic_comb$coef,y=y.test.nonoise, method='kendall')
   score.rank.logistic <- cor(x.test %*% fit_logistic$coef,y=y.test.nonoise, method='kendall')
 
-  c(score=c(score.class.hinge.trans,score.class.hinge,score.class.logistic.trans,score.class.logistic,
-            score.rank.hinge.trans,score.rank.hinge,score.rank.logistic.trans,score.rank.logistic), pvalue=c(fit_hinge_trans$pvalue, fit_hinge$pvalue, fit_logistic_trans$pvalue, fit_logistic$pvalue), coefAN = c(fit_hinge_trans$coefAN,fit_hinge$coefAN, fit_logistic_trans$coefAN, fit_logistic$coefAN), sdAN=c(1.96*fit_hinge_trans$sigmaAN/sqrt(nobs), 1.96*fit_hinge$sigmaAN/sqrt(nobs), 1.96*fit_logistic_trans$sigmaAN/sqrt(nobs), 1.96*fit_logistic$sigmaAN/sqrt(nobs)))
+
+  c(score=c(score.class.logistic.trans,score.class.logistic.comb,score.class.logistic,
+            score.rank.logistic.trans,score.rank.logistic.comb,score.rank.logistic), pvalue=c(fit_logistic_trans$pvalue, fit_logistic_comb$pvalue, fit_logistic$pvalue))
   }))
-  apply(res[,1:8], 2, mean)
-  apply(res[,9:40],2,function(t){mean(t<0.05)})
-  coef <- apply(res[,41:72],2,mean)
-  #sd <- apply(res[,17:32],2,sd)
-  apply(apply(res[,41:104],1,function(t){abs(t[1:32]-coef)<t[33:64]}),1,mean)
-  save(res, file=paste0("~/Simulations/concordLearn/sim","_",sample.size,"_",p,"_transfer.RData"))
+  apply(res[,1:6], 2, mean, na.rm = TRUE)
+  apply(res[,7:30],2,function(t){mean(t<0.05)})
+  save(res, file=paste0("~/Simulations/concordLearn/sim","_",sample.size,"_",p,"_",alpha,"_transfer.RData"))
 }
 
 library(doParallel)
@@ -48,8 +55,12 @@ n_cores <- detectCores(all.tests = FALSE, logical = TRUE)
 cl <- makeCluster(n_cores)
 registerDoParallel(cl)
 
-simulation(sample.size = 200, p=500)
-simulation(sample.size = 350, p=500)
-simulation(sample.size = 500, p=500)
+alpha_seq <- c(0,0.25,0.5,0.75,1)
+for (alpha in alpha_seq){
+  simulation(sample.size = 200, p=500, alpha = alpha)
+  simulation(sample.size = 350, p=500, alpha = alpha)
+  simulation(sample.size = 500, p=500, alpha = alpha)
+}
+
 
 stopCluster(cl)
